0e084360f4f1b9665a446990c8596c9523ba6963
riccardo85 <riccardopiccione@libero.it>
2017-07-06 16:39:37 +0200

    Add files via upload

---
+++ codiceFiscale/src/main/java/codiceFiscale/codiceFiscale/App.java
@@ -0,0 +1,13 @@
+package codiceFiscale.codiceFiscale;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
